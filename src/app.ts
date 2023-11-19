20d3a45b036acbbdcb2f919bf9fc09b01fb2505b
riyad3399 <riyad3399>
2023-11-19 12:50:39 +0600

    basic typescript project setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const apple = 10;
+
+  res.send(apple);
+});
+
+export default app;
