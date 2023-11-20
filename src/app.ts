9aebe43abaf92ad93debb1b704429bbf44d062e6
riyad3399 <riyad3399>
2023-11-20 22:54:51 +0600

    setup moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes} from './app/modules/student/student.route';
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const apple = 10;
+// application routes
+app.use("/api/v1/students", StudentRoutes)
 
-  res.send(apple);
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Typescript first project is running..");
 });
 
 export default app;
